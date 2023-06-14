77369e349d3bcc438465f5a4e8601009fc006fe3
kartik0323 <kartikpoojary8@gmail.com>
2023-06-15 00:17:17 +0530

    myportfolio

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am an IT Engineer studying at Vidyavardhini's college of engineering and technology"
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
